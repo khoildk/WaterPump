4432978fbaafbcb1d9d6a74cb54a010502ed4006
khoildk <23520765@gm.uit.edu.vn>
2025-10-06 10:42:13 +0700

    Add files via upload

---
+++ Source/Core/relay.h
@@ -0,0 +1,19 @@
+/*
+ * relay.h
+ *
+ *  Created on: Oct 6, 2025
+ *      Author: Admin
+ */
+
+#ifndef INC_RELAY_H_
+#define INC_RELAY_H_
+
+#include "main.h"
+
+void Relay_Init(void);
+void Relay_On(void);
+void Relay_Off(void);
+
+
+
+#endif /* INC_RELAY_H_ */
