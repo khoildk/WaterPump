12fef68c8156fc94c11d5667ab3780f11ba16876
khoildk <23520765@gm.uit.edu.vn>
2025-11-10 18:43:00 +0700

    Update relay.h

--- Source/Core/Inc/relay.h
+++ Source/Core/Inc/relay.h
@@ -1,19 +1,17 @@
-/*
- * relay.h
- *
- *  Created on: Oct 6, 2025
- *      Author: Admin
- */
-
 #ifndef INC_RELAY_H_
 #define INC_RELAY_H_
 
-#include "main.h"
+#include "stm32f1xx_hal.h"
 
-void Relay_Init(void);
-void Relay_On(void);
-void Relay_Off(void);
+// Cấu trúc lưu thông tin relay
+typedef struct {
+    GPIO_TypeDef *GPIOx;   // Cổng GPIO
+    uint16_t Pin;          // Chân GPIO
+} Relay_HandleTypeDef;
 
-
+// Các hàm điều khiển relay
+void Relay_Init(Relay_HandleTypeDef *hrelay, GPIO_TypeDef *GPIOx, uint16_t Pin);
+void Relay_On(Relay_HandleTypeDef *hrelay);
+void Relay_Off(Relay_HandleTypeDef *hrelay);
 
 #endif /* INC_RELAY_H_ */
